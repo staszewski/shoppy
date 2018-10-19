04509c323eacbba499affac2bb718bfc3c5bff4f
KamilStaszewski <kamilstaszewski95@gmail.com>
2018-10-19 06:32:16 +0200

    starting ant grid

--- client/src/App/Components/SearchBars/SearchBars.js
+++ client/src/App/Components/SearchBars/SearchBars.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Input from 'antd/lib/input';
+import Icon from 'antd/lib/icon';
+import Button from 'antd/lib/button';
 
 const SearchBars = () => {
   return (
     <div className="searchbars">
         <Input placeholder="What are you looking for today?" id="searchbars__item"/>
         <Input placeholder="Location" id="searchbars__location"/>
+        <Button type="primary" size="small" id="searchbars__confirm">Post an item</Button>
     </div>
   )
 }
--- client/src/App/Components/TopBar/TopBar.js
+++ client/src/App/Components/TopBar/TopBar.js
@@ -1,20 +1,15 @@
 import React from 'react';
+import { Row, Col } from 'antd';
 import Button from 'antd/lib/button';
 
 const TopBar = () => {
   return (
-    <div className="topbar">
-        <div className="topbar__logocontainer">
-            <img src="Images/store.svg" className="topbar__logo" alt="logo"/>
-            <div className="topbar__logotext">
-              <p>Shoppy</p>
-            </div>
-        </div>
-        <div className="topbar__user">
-          <img src="Images/user.svg" className="topbar__useravatar" alt="logo"/>
-          <p>Dashboard</p>
-          <Button type="primary" size="large">Post an item</Button>
-        </div>
+    <div>
+      <Row>
+        <Col span={12}>
+        coś
+        </Col>
+      </Row>
     </div>
   )
 }
