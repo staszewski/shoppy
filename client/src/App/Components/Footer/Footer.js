4d3de96bb6bee5cd9b049b52f73648b8b3fae94c
Kamil Staszewski <kamilstaszewski95@gmail.com>
2018-10-25 19:07:19 +0200

    fixed all the console conflicts

--- client/src/App/Components/Footer/Footer.js
+++ client/src/App/Components/Footer/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
           <Col xs={24}>
             <div className="footer__content">
                 <p>Coded by Kamil Staszewski</p>
-                <div className="footer__credit">Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+                <div className="footer__credit">Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC 3.0 BY</a></div>
             </div>
           </Col>
       </Row>
--- client/src/App/Components/ShopList/CategoriesBar.js
+++ client/src/App/Components/ShopList/CategoriesBar.js
@@ -8,22 +8,22 @@ const CategoriesBar = ({ handleCategories }) => {
       <Row>
           <Col xs={0} md={12} xl={6}>
           <div className="categoriesbar__categories">
-            <Link to="/Toys"><img src="./Images/toy.svg"/>Toys</Link>
+            <Link to="/Toys"><img src="./Images/toy.svg" alt="Toys category"/>Toys</Link>
           </div>
           </Col>
           <Col xs={0} md={12} xl={6}>
           <div className="categoriesbar__categories">
-            <Link to="/Cars"><img src="./Images/car.svg" />Cars</Link>
+            <Link to="/Cars"><img src="./Images/car.svg" alt="Cars category"/>Cars</Link>
           </div>
           </Col>
           <Col xs={0} md={12} xl={6}>
           <div className="categoriesbar__categories">
-            <Link to="/Property"><img src="./Images/property.svg" />Property</Link>
+            <Link to="/Property"><img src="./Images/property.svg" alt="Property category"/>Property</Link>
           </div>
           </Col>
           <Col xs={0} md={12} xl={6}>
           <div className="categoriesbar__categories">
-            <Link to="/Electronics"><img src="./Images/electronics.svg" />Electronics</Link>
+            <Link to="/Electronics"><img src="./Images/electronics.svg" alt="Electronics category"/>Electronics</Link>
           </div>
           </Col>
       </Row>      
