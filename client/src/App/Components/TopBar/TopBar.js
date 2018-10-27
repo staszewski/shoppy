b679c176fbe5f15a987eb4ed26679f77ef94618a
unknown <ruheallein@gmail.com>
2018-10-27 17:21:59 -0300

    started implementation of burger button

--- client/src/App/Components/TopBar/TopBar.js
+++ client/src/App/Components/TopBar/TopBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Row, Col } from 'antd';
 import Button from 'antd/lib/button';
 import { Link } from 'react-router-dom';
+import { slide as Menu } from 'react-burger-menu'
 
 const TopBar = () => {
   return (
@@ -15,11 +16,19 @@ const TopBar = () => {
         </Col>
         <Col xs={0} md={10} lg={8} xl={4}>
         <div className="topbar__user">
+		
           <img src="Images/user.svg" className="topbar__useravatar" alt="logo"/>
           <p>Dashboard</p>
           <Button type="primary" ghost>Log in</Button>
         </div>
         </Col>
+		<Col>
+			<button class="hamburger hamburger--collapse" type="button">
+				<span class="hamburger-box">
+					<span class="hamburger-inner"></span>
+				</span>
+			</button>
+		</Col>
       </Row>
     </div>
   )
