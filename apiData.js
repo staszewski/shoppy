9d9d5c0d065060aebbaa63446d4a25e25cdd8faa
Kamil Staszewski <kamilstaszewski95@gmail.com>
2018-10-29 06:34:01 +0100

    trying dynamic routing for items

--- apiData.js
+++ apiData.js
@@ -6,6 +6,7 @@ const data = [{
     },
     product: {
         name: "Sony Playstation 3",
+        id: 1,
         description: "Lorem ipsum dolor sit amet.",
         price: "100EUR",
         img: "https://img01-olxpl.akamaized.net/img-olxpl/762384094_1_644x461_sony-playstation-2-fajny-zestaw-10-gier-przemysl_rev002.jpg",
--- index.js
+++ index.js
@@ -10,27 +10,34 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/:category', function (req, res) {
 	let find = data.filter((el => {
-	  return el.product.category == req.params.category
+		return el.product.category == req.params.category
+	}));
+	res.json(find);
+	console.log('found category');
+});
+
+app.get('item/:id', function (req, res) {
+	let find = data.filter((el => {
+		return el.product.id == req.params.id
 	}))
 	res.json(find)
-	console.log('found category')
-  })
-  
+	console.log('found item')
+})
 
 // An api endpoint that returns a short list of items
-app.get('/api/data', (req,res) => {
+app.get('/api/data', (req, res) => {
 	var questions = data
 	res.json(questions);
 	console.log('Sent list of items');
-	
+
 });
 
 
 
 
 // Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-	res.sendFile(path.join(__dirname+'/client/public/index.html'));
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname + '/client/public/index.html'));
 });
 
 const port = process.env.PORT || 5000;
